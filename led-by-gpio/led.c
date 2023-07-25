4ceb1b93b0f1ead7fde8560edeecf1ba76cb63e5
Daniel Peczek <danpeczek@gmail.com>
2023-07-25 21:48:28 +0200

    Turn on led by gpio (#2)
    * started
    
    * gpio code for blinking led

---
+++ led-by-gpio/led.c
@@ -0,0 +1,26 @@
+#include <gpiod.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+	printf("Gpiod version %s\n", gpiod_version_string());
+	
+	struct gpiod_chip* chip;
+	struct gpiod_line* led;
+	
+	chip = gpiod_chip_open("/dev/gpiochip0");
+
+	led = gpiod_chip_get_line(chip, 35);
+
+	gpiod_line_request_output(led, "example", 0);
+	
+	for (int i = 0; i < 10; ++i) {
+		gpiod_line_set_value(led, i%2);
+		usleep(100000);
+	}
+
+	gpiod_line_set_value(led, 0);
+	gpiod_line_release(led);
+	gpiod_chip_close(chip);
+	return 0;
+}
